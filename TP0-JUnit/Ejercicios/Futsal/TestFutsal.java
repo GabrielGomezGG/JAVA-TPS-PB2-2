3f32f90a649bad384eb198a8a167f88c1dc15c22
GabrielGomezGG <gabrielgomezgg1997@gmail.com>
2021-10-21 23:08:42 -0300

    TP0 Futsal: add TestFutsal

---
+++ TP0-JUnit/Ejercicios/Futsal/TestFutsal.java
@@ -0,0 +1,14 @@
+package Futsal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestFutsal {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
